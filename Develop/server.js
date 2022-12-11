ccb2cd511fc05d2c5dd863494f486a20534aeb36
Grace <112779767+grachimus@users.noreply.github.com>
2022-12-11 14:25:50 -0600

    fixed wildcard route

--- Develop/server.js
+++ Develop/server.js
@@ -18,18 +18,13 @@ app.use('/api', api);
 app.use(express.static('public'));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
+app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET Route for notes page
 app.get('/notes', (req,res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-// Wildcard route to direct users to a 404 page
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/404.html'))
 );
 
 //PORT where app is hosted
